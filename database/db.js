d70b022963e06ba2d5d1a377f528f71cf80761fd
AREXANS <89494133+AREXANS@users.noreply.github.com>
2022-05-28 11:22:03 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://arexans-web.herokuapp.com';
+var url = 'https://api.arexans.com';
 try {
-if(url == 'https://arexans-web.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.arexans.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
